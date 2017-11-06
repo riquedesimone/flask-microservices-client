67d65983d0bb786c578c919abb11ae5f5d1885ed
Henrique De Simone <eu@henriquedesimone.com.br>
2017-11-05 22:08:20 -0200

    Add docker

---
+++ Dockerfile
@@ -0,0 +1,14 @@
+FROM node:last
+
+RUN mkdir /usr/src/app
+WORKDIR /usr/src/app
+
+ENV PATH /usr/src/app/node_modules/.bin:$PATH
+
+ADD package.json /usr/src/app/package.json
+RUN npm install --silent
+RUN nom install react-scripts -g --silent
+
+ADD . /usr/src/app
+
+CMD ["npm", "start"]
--- src/index.js
+++ src/index.js
@@ -2,13 +2,16 @@ import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 import axios from 'axios';
 import UsersList from './components/UsersList';
+import AddUser from './components/AddUser';
 
 class App extends Component {
   constructor(){
     super()
 
     this.state = {
-      users: []
+      users: [],
+      username: '',
+      email: ''
     }
   }
 
@@ -24,6 +27,12 @@ class App extends Component {
             <br/>
             <h1>All Users</h1>
             <hr/><br/>
+              <AddUser
+                username={this.state.username}
+                email={this.state.email}
+                handleChange={this.handleChange.bind(this)}
+                addUser={this.addUser.bind(this)}/>
+            <br/>
               <UsersList users={this.state.users}/>
           </div>
         </div>
@@ -35,6 +44,27 @@ class App extends Component {
     axios.get(`${process.env.REACT_APP_USERS_SERVICE_URL}/users`)
       .then((res) => { this.setState({users:res.data.data.users}); })
       .catch((err) => { console.log(err); })
+  }
+
+  addUser(event) {
+    event.preventDefault();
+    const data = {
+      username: this.state.username,
+      email: this.state.email
+    }
+
+    axios.post(`${process.env.REACT_APP_USERS_SERVICE_URL}/users`, data)
+      .then((res) => {
+        this.getUsers();
+        this.setState({username:'', email:''})
+      })
+      .catch((err) => {console.log(err);})
+  }
+
+  handleChange(event) {
+    const obj = {};
+    obj[event.target.name] = event.target.value;
+    this.setState(obj);
   }
 }
 
